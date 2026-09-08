7beaa95dd7ce43abd539f170f03f8fbd8e22f772
Paul Deister <88551184+PaulDeister@users.noreply.github.com>
2026-09-08 14:56:12 +0200

    chore: fix pre-existing eslint errors so the lint job can gate

--- test/lib/resolve-bindings.test.js
+++ test/lib/resolve-bindings.test.js
@@ -12,26 +12,26 @@ class NobleMac {}
 class NobleWinrt {}
 
 const NobleMacImport = proxyquire('../../lib/mac/bindings', {
-  bindings: () => ({ NobleMac }),
+  bindings: () => ({ NobleMac })
 });
 
 const NobleWinrtImport = proxyquire('../../lib/win/bindings', {
-  bindings: () => ({ NobleWinrt }),
+  bindings: () => ({ NobleWinrt })
 });
 
 const WebSocket = require('../../lib/websocket/bindings');
 const NobleBindings = proxyquire('../../lib/distributed/bindings', {
-  ws: { Server: EventEmitter },
+  ws: { Server: EventEmitter }
 });
 const HciNobleBindings = proxyquire('../../lib/hci-socket/bindings', {
-  './hci': EventEmitter,
+  './hci': EventEmitter
 });
 const resolver = proxyquire('../../lib/resolve-bindings', {
   './distributed/bindings': NobleBindings,
   './hci-socket/bindings': HciNobleBindings,
   './mac/bindings': NobleMacImport,
   './win/bindings': NobleWinrtImport,
-  os: { platform, release },
+  os: { platform, release }
 });
 
 describe('resolve-bindings', () => {
